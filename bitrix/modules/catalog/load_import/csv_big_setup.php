a6fb58ce27f3f5a9705c60db90ca07233cfe195f
rakatanui <grafgarrsky@gmail.com>
2017-05-22 13:53:35 +0300

    'ImportAdmin-Good'

--- bitrix/modules/catalog/load_import/csv_big_setup.php
+++ bitrix/modules/catalog/load_import/csv_big_setup.php
@@ -1,7 +1,8 @@
 <?
 //<title>CSV (lifesport)</title>
 use Bitrix\Main;
-IncludeModuleLangFile($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/catalog/import_setup_templ.php');
+
+IncludeModuleLangFile($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/catalog/import_setup_templ.php');
 /** @global string $ACTION */
 /** @global string $URL_DATA_FILE */
 /** @global string $DATA_FILE_NAME */
@@ -17,43 +18,42 @@ global $APPLICATION, $USER;
 
 $NUM_CATALOG_LEVELS = (int)Main\Config\Option::get('catalog', 'num_catalog_levels');
 if ($NUM_CATALOG_LEVELS <= 0)
-	$NUM_CATALOG_LEVELS = 3;
+    $NUM_CATALOG_LEVELS = 3;
 
 $arSetupErrors = array();
 
 global
-	$arCatalogAvailProdFields,
-	$defCatalogAvailProdFields,
-	$arCatalogAvailPriceFields,
-	$defCatalogAvailPriceFields,
-	$arCatalogAvailValueFields,
-	$defCatalogAvailValueFields,
-	$arCatalogAvailQuantityFields,
-	$defCatalogAvailQuantityFields,
-	$arCatalogAvailGroupFields,
-	$defCatalogAvailGroupFields,
-	$defCatalogAvailCurrencies;
+$arCatalogAvailProdFields,
+$defCatalogAvailProdFields,
+$arCatalogAvailPriceFields,
+$defCatalogAvailPriceFields,
+$arCatalogAvailValueFields,
+$defCatalogAvailValueFields,
+$arCatalogAvailQuantityFields,
+$defCatalogAvailQuantityFields,
+$arCatalogAvailGroupFields,
+$defCatalogAvailGroupFields,
+$defCatalogAvailCurrencies;
 
 //********************  ACTIONS  **************************************//
-if (($ACTION == 'IMPORT_EDIT' || $ACTION == 'IMPORT_COPY') && $STEP == 1)
-{
-	if (isset($arOldSetupVars['IBLOCK_ID']))
-		$IBLOCK_ID = $arOldSetupVars['IBLOCK_ID'];
-	if (isset($arOldSetupVars['URL_DATA_FILE']))
-		$URL_DATA_FILE = $arOldSetupVars['URL_DATA_FILE'];
-	if (isset($arOldSetupVars['DATA_FILE_NAME']))
-		$DATA_FILE_NAME = $arOldSetupVars['DATA_FILE_NAME'];
+if (($ACTION == 'IMPORT_EDIT' || $ACTION == 'IMPORT_COPY') && $STEP == 1) {
+    if (isset($arOldSetupVars['IBLOCK_ID']))
+        $IBLOCK_ID = $arOldSetupVars['IBLOCK_ID'];
+    if (isset($arOldSetupVars['URL_DATA_FILE']))
+        $URL_DATA_FILE = $arOldSetupVars['URL_DATA_FILE'];
+    if (isset($arOldSetupVars['DATA_FILE_NAME']))
+        $DATA_FILE_NAME = $arOldSetupVars['DATA_FILE_NAME'];
 }
 
 //********************  END ACTIONS  **********************************//
 
 $aMenu = array(
-	array(
-		"TEXT"=>GetMessage("CATI_ADM_RETURN_TO_LIST"),
-		"TITLE"=>GetMessage("CATI_ADM_RETURN_TO_LIST_TITLE"),
-		"LINK"=>"/bitrix/admin/cat_import_setup.php?lang=".LANGUAGE_ID,
-		"ICON"=>"btn_list",
-	)
+    array(
+        "TEXT" => GetMessage("CATI_ADM_RETURN_TO_LIST"),
+        "TITLE" => GetMessage("CATI_ADM_RETURN_TO_LIST_TITLE"),
+        "LINK" => "/bitrix/admin/cat_import_setup.php?lang=" . LANGUAGE_ID,
+        "ICON" => "btn_list",
+    )
 );
 
 $context = new CAdminContextMenu($aMenu);
@@ -61,79 +61,96 @@ $context = new CAdminContextMenu($aMenu);
 $context->Show();
 
 if (!empty($arSetupErrors))
-	ShowError(implode('<br>', $arSetupErrors));
+    ShowError(implode('<br>', $arSetupErrors));
 ?>
 <!--suppress JSUnresolvedVariable -->
-<form method="POST" action="<? echo $APPLICATION->GetCurPage(); ?>" ENCTYPE="multipart/form-data" name="dataload">
-<?
-$aTabs = array(
-	array("DIV" => "edit1", "TAB" => GetMessage("CAT_ADM_CSV_IMP_TAB1"), "ICON" => "store", "TITLE" => GetMessage("CAT_ADM_CSV_IMP_TAB1_TITLE")),
-);
-
-$tabControl = new CAdminTabControl("tabControl", $aTabs, false, true);
-$tabControl->Begin();
-
-$tabControl->BeginNextTab();
-
-if ($STEP == 1)
-{
-	?><tr class="heading">
-		<td colspan="2"><? echo GetMessage("CATI_DATA_LOADING"); ?></td>
-	</tr>
-	<tr>
-		<td valign="top" width="40%"><? echo GetMessage("CATI_DATA_FILE_SITE"); ?>:</td>
-		<td valign="top" width="60%">
-			<input type="text" name="URL_DATA_FILE" size="40" value="<? echo htmlspecialcharsbx($URL_DATA_FILE); ?>">
-			<input type="button" value="<? echo GetMessage("CATI_BUTTON_CHOOSE")?>" onclick="cmlBtnSelectClick();"><?
-CAdminFileDialog::ShowScript(
-	array(
-		"event" => "cmlBtnSelectClick",
-		"arResultDest" => array("FORM_NAME" => "dataload", "FORM_ELEMENT_NAME" => "URL_DATA_FILE"),
-		"arPath" => array("PATH" => "/upload/catalog", "SITE" => SITE_ID),
-		"select" => 'F',// F - file only, D - folder only, DF - files & dirs
-		"operation" => 'O',// O - open, S - save
-		"showUploadTab" => true,
-		"showAddToMenuTab" => false,
-		"fileFilter" => 'csv',
-		"allowAllFiles" => true,
-		"SaveConfig" => true
-	)
-);
-		?></td>
-	</tr>
-    <tr>
-		<td valign="top" width="40%">
-            <input type="submit" value="Сохранить">
-        </td>
-        <td valign="top" width="60%"></td>
-	</tr>
+<form method="POST" action="<? /* echo $APPLICATION->GetCurPage(); */ ?>" ENCTYPE="multipart/form-data" name="dataload">
     <?
-}
+    $aTabs = array(
+        array("DIV" => "edit1", "TAB" => GetMessage("CAT_ADM_CSV_IMP_TAB1"), "ICON" => "store", "TITLE" => GetMessage("CAT_ADM_CSV_IMP_TAB1_TITLE")),
+    );
 
-$tabControl->EndTab();
+    $tabControl = new CAdminTabControl("tabControl", $aTabs, false, true);
+    $tabControl->Begin();
 
-$tabControl->BeginNextTab();
+    $tabControl->BeginNextTab();
+
+    if ($STEP == 1) {
+        ?>
+        <tr class="heading">
+        <td colspan="2"><? echo GetMessage("CATI_DATA_LOADING"); ?></td>
+        </tr>
+        <tr>
+            <td valign="top" width="40%"><? echo GetMessage("CATI_DATA_FILE_SITE"); ?>:</td>
+            <td valign="top" width="60%">
+                <input type="text" name="URL_DATA_FILE" size="40"
+                       value="<? echo htmlspecialcharsbx($URL_DATA_FILE); ?>">
+                <input type="button" value="<? echo GetMessage("CATI_BUTTON_CHOOSE") ?>"
+                       onclick="cmlBtnSelectClick();"><?
+                CAdminFileDialog::ShowScript(
+                    array(
+                        "event" => "cmlBtnSelectClick",
+                        "arResultDest" => array("FORM_NAME" => "dataload", "FORM_ELEMENT_NAME" => "URL_DATA_FILE"),
+                        "arPath" => array("PATH" => "/upload/catalog", "SITE" => SITE_ID),
+                        "select" => 'F',// F - file only, D - folder only, DF - files & dirs
+                        "operation" => 'O',// O - open, S - save
+                        "showUploadTab" => true,
+                        "showAddToMenuTab" => false,
+                        "fileFilter" => 'csv',
+                        "allowAllFiles" => true,
+                        "SaveConfig" => true
+                    )
+                );
+                ?></td>
+        </tr>
+        <tr>
+            <td valign="top" width="40%">
+                <input type="submit" value="Сохранить">
+            </td>
+            <td valign="top" width="60%"></td>
+        </tr>
+        <?
+    }
+
+    $tabControl->EndTab();
+
+    ?>
+</form>
+
+<?php
+
+$array = array();
+$fileopen = fopen($_SERVER['DOCUMENT_ROOT'] . $_POST["URL_DATA_FILE"], "r");
+$hendler = fgetcsv($fileopen, "0", ";", '"');
+
+/*while (($mas = fgetcsv($fileopen, 0, ";")) != false) {
+    $handler = fgetcsv($fileopen, 0, ";");
+    $push = array_push($array, $handler);
+}*/
 
 ?>
-</form>
+<pre>
+<?php
+var_dump($hendler);
+?>
+    </pre>
 
 <script type="text/javascript">
 
-function showTranslitSettings()
-{
-	var useTranslit = BX('USE_TRANSLIT_Y'),
-		translitLang = BX('tr_TRANSLIT_LANG'),
-		translitUpdate = BX('tr_USE_UPDATE_TRANSLIT');
-	if (!BX.type.isElementNode(useTranslit) || !BX.type.isElementNode(translitLang) || !BX.type.isElementNode(translitUpdate))
-		return;
-	BX.style(translitLang, 'display', (useTranslit.checked ? 'table-row' : 'none'));
-	BX.style(translitUpdate, 'display', (useTranslit.checked ? 'table-row' : 'none'));
-}
-BX.ready(function(){
-	var useTranslit = BX('USE_TRANSLIT_Y'),
-		translitLang = BX('tr_TRANSLIT_LANG'),
-		translitUpdate = BX('tr_USE_UPDATE_TRANSLIT');
-	if (BX.type.isElementNode(useTranslit) && BX.type.isElementNode(translitLang) && BX.type.isElementNode(translitUpdate))
-		BX.bind(useTranslit, 'click', showTranslitSettings);
-});
+    function showTranslitSettings() {
+        var useTranslit = BX('USE_TRANSLIT_Y'),
+            translitLang = BX('tr_TRANSLIT_LANG'),
+            translitUpdate = BX('tr_USE_UPDATE_TRANSLIT');
+        if (!BX.type.isElementNode(useTranslit) || !BX.type.isElementNode(translitLang) || !BX.type.isElementNode(translitUpdate))
+            return;
+        BX.style(translitLang, 'display', (useTranslit.checked ? 'table-row' : 'none'));
+        BX.style(translitUpdate, 'display', (useTranslit.checked ? 'table-row' : 'none'));
+    }
+    BX.ready(function () {
+        var useTranslit = BX('USE_TRANSLIT_Y'),
+            translitLang = BX('tr_TRANSLIT_LANG'),
+            translitUpdate = BX('tr_USE_UPDATE_TRANSLIT');
+        if (BX.type.isElementNode(useTranslit) && BX.type.isElementNode(translitLang) && BX.type.isElementNode(translitUpdate))
+            BX.bind(useTranslit, 'click', showTranslitSettings);
+    });
 </script>
